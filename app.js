29a86e45365053ba6ecfc38012afb869b05466f2
Heba M. Abdelrehem <hebaabdelrehem2015@gmail.com>
2022-04-17 16:56:49 +0200

    using js core modules

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // console.log('hello from my node js app');
 const helpers = require('./helpers');
+const http = require('http');
+
 // var let const
 const total = helpers.sum(10, 200);
 console.log("Total: ",total);
 // console.log("process:",process);
+
+const server = http.createServer((req,res) => {
+    res.end("hello from node js");
+});
+
+server.listen(3000);
