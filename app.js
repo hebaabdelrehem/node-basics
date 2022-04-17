e08001ff28ec2c1e7de1f4ee693732d63e30dfd1
Heba M. Abdelrehem <hebaabdelrehem2015@gmail.com>
2022-04-17 22:16:13 +0200

    using node packages

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const helpers = require('./helpers');
 const http = require('http');
 
 // var let const
-const total = helpers.sum(10, 200);
+const total = helpers.sum(100, 200);
 console.log("Total: ",total);
 // console.log("process:",process);
 
 const server = http.createServer((req,res) => {
-    res.end("hello from node js");
+    res.end("hello from node js application real updates");
 });
 
 server.listen(3000);
