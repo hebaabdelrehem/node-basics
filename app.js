21b608245c6f29171a0f1ef9d4a66dcc554b4e32
Heba M. Abdelrehem <hebaabdelrehem2015@gmail.com>
2022-04-17 16:30:45 +0200

    first push

---
+++ app.js
@@ -0,0 +1,6 @@
+// console.log('hello from my node js app');
+const helpers = require('./helpers');
+// var let const
+const total = helpers.sum(10, 200);
+console.log("Total: ",total);
+// console.log("process:",process);
---
+++ helpers.js
@@ -0,0 +1,17 @@
+// This is called the regular function
+// function sum(a,b){
+//     return a + b;
+// }
+
+// This is called the arraw function
+// const sum = (a, b) => {
+//     return a + b;
+// };
+
+// const sum = (a, b) => a + b;
+
+// module.exports={
+//     sum
+// };
+
+exports.sum = (a, b) => a + b;
